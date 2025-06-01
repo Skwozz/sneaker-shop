2a6242f06c834c7925fef15ae07f1552f1bdf6b9
Skwozz <rakalov.s@ya.ru>
2025-06-01 10:43:07 +0300

    Добавлен роут delete-cart-item, допилен фронт

--- app/routers/front_router.py
+++ app/routers/front_router.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.db.base import get_session
 from app.models.user import User
 from app.routers.auth_router import get_current_user
-from app.crud.cart_item_crud import create_cart_item, get_cart_item
+from app.crud.cart_item_crud import create_cart_item, get_cart_item, delete_cart_item
 from app.routers.login_router import get_current_user_browser
 from app.schemas.cart_item import CartItemCreate
 
@@ -44,3 +44,15 @@ async def cart_page(request: Request,
     return templates.TemplateResponse('cart.html',
                                     {'request':request,
                                      'items': cart_item})
+
+@router.post('/delete-cart-item/')
+async def delete_cart_item_front(
+        item_id: int = Form(...),
+        session: AsyncSession = Depends(get_session),
+        current_user: User = Depends(get_current_user_browser)
+):
+    await delete_cart_item(session, item_id, current_user.id)
+    return RedirectResponse(url='/cart', status_code=303)
+
+
+
